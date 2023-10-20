7409abaa7765ea483a75b4e390457bc74f84f73a
samuelochaba <nerdysam5826@gmail.com>
2023-10-20 20:14:55 +0100

    __

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+  <div>
+    <form method="POST">
+        <input name='email' placeholder="email" />
+        <input name='password' placeholder="password" />
+        <input name='passwordConfirmation' placeholder="password confirmation" />
+        <button>Sign Up</button>
+    </form>
+   </div>
+  `);
+});
+
+app.post("/", (req, res) => {
+  req.on("data", (data) => console.log(data.toString("utf8")));
+  res.send("Account created!!!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening...");
+});
