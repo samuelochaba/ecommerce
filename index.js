160e4beda42a7cc7ebf096c0733abc2bf2dd3bc2
samuelochaba <nerdysam5826@gmail.com>
2023-10-21 12:27:41 +0100

    auth

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const usersRepo = require("./repositories/users");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,7 +18,21 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must match");
+  }
+
+  const user = await usersRepo.create({ email, password });
+
   res.send(req.body);
 });
 
